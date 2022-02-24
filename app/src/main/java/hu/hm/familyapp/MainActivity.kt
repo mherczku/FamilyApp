46e60b47c55cd7aed5a989bd8a9e83e2990baf51
Mate <mherczku@gmail.com>
2022-02-24 18:58:08 +0100

    Initial commit

---
+++ app/src/main/java/hu/hm/familyapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package hu.hm.familyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import hu.hm.familyapp.ui.navigation.ActivityScreen
+import hu.hm.familyapp.ui.navigation.NavScreen
+import hu.hm.familyapp.ui.theme.FamilyAppTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FamilyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ActivityScreen()
+                }
+            }
+        }
+    }
+}
